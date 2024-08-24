13a91230c83aa40cea37e145e453040d846a12be
ZULFIQAR BALOCH <mhbking0340@gmail.com>
2024-08-24 18:24:27 +0500

    Add files via upload

---
+++ MHB.py
@@ -0,0 +1,9 @@
+#-created BY ZULFIQAR BALOCH
+import os
+os.system('git pull')
+print("join Whatsapp Group")
+os.system('xdg-open https://chat.whatsapp.com/HdppaiQyFtZ1AwiOzc5iEe')
+import MHB
+
+
+               
