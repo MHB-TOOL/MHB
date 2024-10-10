e9608e20a57b9130a5c044bd2182888f5191b63c
ZULFIQAR BALOCH <mhbking0340@gmail.com>
2024-10-10 13:19:19 +0500

    Add files via upload

--- MHB.py
+++ MHB.py
@@ -4,12 +4,12 @@ try:
  import requests
 except:os.system("pip uninstall requests -y;pip install requests")
 
-print('\033[1;91m[\033[1;97m-\033[1;91m] \033[1;97mChecking For Update...')
+print('[*] Loeading Chacking Update.)
 os.system('git pull --quiet 2>/dev/null')
 bit = platform.architecture()[0]
 if bit == '64bit':
- print('\033[1;91m[\033[1;97m✓\033[1;97m] \033[1;97m64Bit Found')
+ print(' [*] \033[1;97m64Bit Found')
  import MHB64
 elif bit == '32bit':
- print('\033[1;91m[\033[1;97m✓\033[1;97m] \033[1;97m32Bit Found')
+ print(' [*] 32Bit Found ')
  import MHB32
