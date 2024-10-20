2e485edb75bd6824298b19290702ffa725529542
ZULFIQAR BALOCH <mhbking0340@gmail.com>
2024-10-20 19:30:40 +0500

    Update MHB.py

--- MHB.py
+++ MHB.py
@@ -11,5 +11,6 @@ if bit == '64bit':
  print('[✓] Found 64 Bit Device')
  import MHB64
 elif bit == '32bit':
- print('\033[1;97m[✓] Found 32 Bit Device')
- import MHB32
+ print('\033[1;97m[✓] SORRY BRO YOUR DEVIS IS 32 BIT ')
+ print('\033[1;97m[✓] PLEASE USE 4=64 DEVIS ')
+ 
